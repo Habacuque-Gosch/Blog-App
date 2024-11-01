535d5bb94887b6615d12784051eb6a1ced95dbb6
habacuque-gosch <habacuke14@gmail.com>
2024-10-31 22:52:20 -0300

    make dir static files

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,14 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 
-
+router.get('/', (req, res) => {
+    res.send('pagina inicial admin')
+})
 
 router.get('/home-admin', (req, res) => {
     res.send('teste admin')
 })
-
-
-
 
 
 module.exports = router
