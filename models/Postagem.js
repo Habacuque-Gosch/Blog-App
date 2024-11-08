c16873dd8ed23fbf74f45997f56c45bc131c2398
habacuque-gosch <habacuke14@gmail.com>
2024-11-08 16:00:22 -0300

    page edit post forms

--- models/Postagem.js
+++ models/Postagem.js
@@ -28,13 +28,14 @@ const Postagem = db.sequelize.define('postagens',{
 )
 
 Postagem.belongsTo(Categoria)
+Categoria.belongsTo(Postagem)
 
 // SYNC MODEL
-// Postagem.sync({force: true}).then(() =>{
-//     console.log('Model postagem criado com sucesso')
-// }).catch((erro) =>{
-//     console.log('erro ao sync tabelas '+erro)
-// })
+Postagem.sync({force: true}).then(() =>{
+    console.log('Model postagem criado com sucesso')
+}).catch((erro) =>{
+    console.log('erro ao sync tabelas '+erro)
+})
 
 // Postagem.create({
 //     nome: "teste js code",
