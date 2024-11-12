b751e5b3faf4cbfcd53f2e6ea68941aa264e95d9
habacuque-gosch <habacuke14@gmail.com>
2024-11-12 19:47:33 -0300

    connect exclude

--- models/db.js
+++ models/db.js
@@ -1,7 +1,6 @@
 // Connnet DB
 const Sequelize = require('sequelize');
 const pg = require('pg')
-// const sequelize = new Sequelize('postgresql://blogapp_owner:UrGvQxS15gbj@ep-soft-brook-a5ns113b.us-east-2.aws.neon.tech/blogapp?sslmode=require', {dialect: pg});
 const sequelize = new Sequelize('postgresql://postgres:2005@127.0.0.1/blog_app', {dialect: pg});
 
 
