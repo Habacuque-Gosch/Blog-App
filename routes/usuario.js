73e79716a4f01865df80373e3a0512ce1ee225f2
habacuque-gosch <habacuke14@gmail.com>
2024-11-12 19:57:49 -0300

    fecth "usuario"

--- routes/usuario.js
+++ routes/usuario.js
@@ -27,27 +27,14 @@ router.all('/login', (req, res) => {
     
     // if(req.method == 'POST') {
 
-
     // } else {
 
-        res.render('users/login')
+    res.render('users/login')
     // }
 })
 
-// router.post('/login/authenticate', 
-//     passport.authenticate('local', { successRedirect: '/admin', failureRedirect: '/users/login' }),
-//     function(req, res) {
-//         console.log(req.body.email)
-//         res.redirect('/');
-//     });
-
-/**
-* @description função responsavel por autenticar o usuario no sistema
-* @param redirect
-* @returns
-*/
 router.post('/login/authenticate',(req, res, next) => {
-    //...
+
     console.log(req.method)
 
     console.log('AUTENTICATE IS ON')
@@ -55,11 +42,9 @@ router.post('/login/authenticate',(req, res, next) => {
         successRedirect: "/",
         failureRedirect: "/users/login",
         failureFlash: true
-    })
-    console.log('AUTENTICATE SUCCESSFULL')
-    res.redirect('/')
-    // next()
+    })(req, res, next)
 
+    console.log('AUTENTICATE SUCCESSFULL')
 
 })
 
