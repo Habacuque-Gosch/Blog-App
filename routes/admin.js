60bc27f227fc14900e10aa75e105a949dad945db
habacuque-gosch <habacuke14@gmail.com>
2024-11-14 17:30:45 -0300

    Apply GPO in index admin pannel

--- routes/admin.js
+++ routes/admin.js
@@ -7,11 +7,11 @@ const {eAdmin} = require('../helpers/eAdmin')
 
 
 
-router.get('/', (req, res) => {
+router.get('/', eAdmin, (req, res) => {
     res.render('admin/')
 })
 
-router.get('/categorias', eAdmin,(req, res) => {
+router.get('/categorias', eAdmin, (req, res) => {
 
     Categoria.findAll({order: [['id', 'DESC']]}).then((categorias) => {
         res.render('admin/categorias', {categorias: categorias})
@@ -21,7 +21,7 @@ router.get('/categorias', eAdmin,(req, res) => {
     })
 })
 
-router.all('/add-categoria', eAdmin,(req, res) => {
+router.all('/add-categoria', eAdmin, (req, res) => {
 
     if (req.method == 'POST'){
 
@@ -57,7 +57,7 @@ router.all('/add-categoria', eAdmin,(req, res) => {
 
 })
 
-router.all('/edit-categoria/:id', eAdmin,(req, res) => {
+router.all('/edit-categoria/:id', eAdmin, (req, res) => {
 
     var id = req.params.id
 
@@ -91,7 +91,7 @@ router.all('/edit-categoria/:id', eAdmin,(req, res) => {
     })
 })
 
-router.get('/delete-categoria/:id', eAdmin,(req, res) => {
+router.get('/delete-categoria/:id', eAdmin, (req, res) => {
     Categoria.destroy({where: {'id': req.params.id}}).then(() => {
         req.flash('success_msg', 'Categoria deletada com sucesso')
         res.redirect('/admin/categorias')
@@ -101,7 +101,7 @@ router.get('/delete-categoria/:id', eAdmin,(req, res) => {
     })
 })
 
-router.get('/postagens', eAdmin,(req, res) => {
+router.get('/postagens', eAdmin, (req, res) => {
     Postagem.findAll(
         {include: [{
             model: Categoria,
@@ -116,7 +116,7 @@ router.get('/postagens', eAdmin,(req, res) => {
     })
 })
 
-router.all('/add-postagens', eAdmin,(req, res) => {
+router.all('/add-postagens', eAdmin, (req, res) => {
     
     Categoria.findAll({order: [['id', 'DESC']]}).then(function(categorias){
 
@@ -159,7 +159,7 @@ router.all('/add-postagens', eAdmin,(req, res) => {
     })
 })
 
-router.all('/edit-postagem/:id', eAdmin,(req,res) => {
+router.all('/edit-postagem/:id', eAdmin, (req,res) => {
     id = req.params.id
 
     Postagem.findOne({where: {'id':id}}, {include: [{
@@ -207,7 +207,7 @@ router.all('/edit-postagem/:id', eAdmin,(req,res) => {
     })
 })
 
-router.get('/delete-postagem/:id', eAdmin,(req, res) => {
+router.get('/delete-postagem/:id', eAdmin, (req, res) => {
     var id_post = req.params.id
     Postagem.destroy({where: {'id': id_post}}).then(() => {
         req.flash('success_msg', 'Postagem deletada com sucesso')
