93b331c5d24327476568d14669e9a3777be0cb64
habacuque-gosch <habacuke14@gmail.com>
2024-11-15 11:05:03 -0300

    alter connect db

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 // Connnet DB
 const Sequelize = require('sequelize');
 const pg = require('pg')
-const sequelize = new Sequelize('postgresql://postgres:2005@127.0.0.1/blog_app', {dialect: pg});
+const sequelize = new Sequelize('postgresql://blog_admin:kHG4QkIIbXMgY8rMFbpdGrtozBN4aj7e@dpg-cspsjql2ng1s7396dqig-a/blog_app_suza', {dialect: pg});
 
 
 
